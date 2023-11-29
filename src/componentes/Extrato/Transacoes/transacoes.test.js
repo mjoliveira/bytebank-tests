6f70da6408d2b5907df6604be2e529cc71864209
mjoliveira <mayara.jesus@acad.pucrs.br>
2023-11-29 19:39:20 -0300

    test: add example test simulating events

--- src/componentes/Extrato/Transacoes/transacoes.test.js
+++ src/componentes/Extrato/Transacoes/transacoes.test.js
@@ -28,3 +28,13 @@ test('Deve renderizar o mesmo componente com props atualizadas', () => {
     expect(novoTipoTransacao).toHaveTextContent('Transferência');
     expect(novoValorTransacao).toHaveTextContent('- R$ 50');
 });
+
+test('Deve chamar um evento de onSubmit ao clicar em realizar transação', () => {
+    const realizarTransacao = jest.fn();
+  
+    render(<Formulario realizarTransacao={realizarTransacao} />);
+    const botao = screen.getByRole('button');
+  
+    userEvent.click(botao);
+    expect(realizarTransacao).toHaveBeenCalledTimes(1);
+});
